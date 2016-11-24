8f1516f122e659207287dd581b464159590c24ff
szj <jbourne123@126.com>
2016-11-24 16:35:54 +0800

    first commit

---
+++ src/BaseRepository.php
@@ -0,0 +1,319 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: szj
+ * Date: 16/8/21
+ * Time: 21:41
+ */
+
+namespace Zyts\Repositories;
+
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class BaseRepository implements BaseRepositoryContract
+{
+    /**
+     * @var Model
+     */
+    protected $modelInstance;
+
+    protected $modelFullName;
+
+    protected $modelName;
+
+    protected $modelNamespace;
+
+    /**
+     * @var Builder
+     */
+    protected $query;
+
+    private $errMsgs = [];
+
+    public function __construct($modelName, $modelNamespace = 'App\\Models\\')
+    {
+        $this->$modelName = $modelName;
+        $this->modelNamespace = $modelNamespace;
+
+        $this->modelFullName = rtrim($modelNamespace, '\\') . '\\' . $modelName;
+        $this->modelInstance = new $this->modelFullName();
+
+        if (env('APP_DEBUG')) {
+            DB::enableQueryLog();
+        }
+    }
+
+    public function addErrMsg($errMsg)
+    {
+        if (trim($errMsg)) {
+            $this->errMsgs[] = $errMsg;
+        }
+    }
+
+    public function getErrMsg()
+    {
+        if ($this->errMsgs)
+            return array_pop($this->errMsgs);
+        return null;
+    }
+
+    public function getSqlDump()
+    {
+        return DB::getQueryLog();
+    }
+
+    public function find($primaryKey, $columns = ['*'])
+    {
+        return $this->modelInstance->find($primaryKey, $columns);
+    }
+
+    public function findOrFail($primaryKey, $columns = ['*'])
+    {
+        return $this->modelInstance->findOrFail($primaryKey, $columns);
+    }
+
+    public function all($columns = ['*'])
+    {
+        return call_user_func_array([$this->modelFullName, 'all'], array($columns));
+    }
+
+    public function getQuery()
+    {
+        if (!$this->query) {
+            $this->query = $this->modelInstance->newQuery();
+        }
+        return $this->query;
+    }
+
+    protected function newQuery()
+    {
+        $this->query = $this->modelInstance->newQuery();
+        return $this->query;
+    }
+
+    public function wheres($wheres)
+    {
+        $this->getQuery();
+        if (arr_is_assoc($wheres)) {
+            foreach ($wheres as $column => $condition) {
+                if (is_array($condition)) {
+                    if ($condition[0] == 'in') {
+                        $this->query->whereIn($column, $condition[1]);
+                    } else {
+                        $this->query->where($column, $condition[0], $condition[1]);
+                    }
+                } else {
+                    $this->query->where($column, $condition);
+                }
+            }
+        } else {
+            foreach ($wheres as $condition) {
+                if (count($condition) == 3) {
+                    if ($condition[1] == 'in') {
+                        $this->query->whereIn($condition[0], $condition[2]);
+                    } else {
+                        $this->query->where($condition[0], $condition[1], $condition[2]);
+                    }
+                } elseif (count($condition) == 2) {
+                    $this->query->where($condition[0], $condition[1]);
+                }
+            }
+        }
+        return $this;
+    }
+
+    public function orWheres($wheres)
+    {
+        $this->getQuery();
+        if (arr_is_assoc($wheres)) {
+            foreach ($wheres as $column => $condition) {
+                if (is_array($condition)) {
+                    $this->query->orWhere($column, $condition[0], $condition[1]);
+                } else {
+                    $this->query->orWhere($column, $condition);
+                }
+            }
+        } else {
+            foreach ($wheres as $condition) {
+                if (count($condition) == 3) {
+                    $this->query->orWhere($condition[0], $condition[1], $condition[2]);
+                } elseif (count($condition) == 2) {
+                    $this->query->orWhere($condition[0], $condition[1]);
+                }
+            }
+        }
+        return $this;
+    }
+
+    public function select($columns)
+    {
+        $this->getQuery();
+        if (is_string($columns)) {
+            $this->query->selectRaw($columns);
+        } else {
+            $this->query->select($columns);
+        }
+        return $this;
+    }
+
+    public function limits($limits)
+    {
+        $this->getQuery();
+        if (is_array($limits)) {
+            $this->query->limit($limits[1])->offset($limits[0]);
+        } else {
+            $this->query->limit($limits);
+        }
+        return $this;
+    }
+
+    public function orders($orders)
+    {
+        $this->getQuery();
+        if (arr_is_assoc($orders)) {
+            foreach ($orders as $column => $order) {
+                $this->query->orderBy($column, $order);
+            }
+        } else {
+            foreach ($orders as $order) {
+                $this->query->orderByRaw($order);
+            }
+        }
+        return $this;
+    }
+
+    public function groups($groups)
+    {
+        $this->getQuery();
+        if (arr_is_assoc()) {
+            foreach ($groups as $column) {
+                $this->query->groupBy($column);
+            }
+        } else {
+            foreach ($groups as $group) {
+                $this->query->groupByRaw($group);
+            }
+        }
+        return $this;
+    }
+
+    public function havings($havings)
+    {
+        $this->getQuery();
+        foreach ($havings as $having) {
+            if (is_string($having)) {
+                $this->query->havingRaw($having);
+            } elseif (is_array($having) && count($having) == 3) {
+                $this->query->having($having[0], $having[1], $having[2]);
+            }
+        }
+        return $this;
+    }
+
+    public function get()
+    {
+        $this->getQuery();
+        $ret = $this->query->get();
+        $this->query = null;
+        return $ret;
+    }
+
+    public function first()
+    {
+        $this->getQuery();
+        $ret = $this->query->first();
+        $this->query = null;
+        return $ret;
+    }
+
+    public function count()
+    {
+        $this->getQuery();
+        $ret = $this->query->count();
+        $this->query = null;
+        return $ret;
+    }
+
+    public function paginate($numPerPage)
+    {
+        $this->getQuery();
+        $ret = $this->query->paginate($numPerPage, ['*'], 'p');
+        $this->query = null;
+        return $ret;
+    }
+
+    public function simpleSelect($wheres, $columns = ['*'], $limits = 0, $orders = [], $groups = [], $havings = [])
+    {
+        $this->newQuery();
+
+        if ($wheres) {
+            $this->wheres($wheres);
+        }
+
+        if ($columns) {
+            $this->select($columns);
+        }
+
+        if ($limits) {
+            $this->limits($limits);
+        }
+
+        if ($orders) {
+            $this->orders($orders);
+        }
+
+        if ($groups) {
+            $this->groups($groups);
+        }
+
+        if ($havings) {
+            $this->havings($havings);
+        }
+
+        return $this->get();
+    }
+
+    public function search($wheres, $columns = ['*'], $limits = 0, $orders = [], $groups = [], $havings = [])
+    {
+        return $this->simpleSelect($wheres, $columns, $limits, $orders, $groups, $havings);
+    }
+
+    public function create($attributes)
+    {
+        return call_user_func_array([$this->modelFullName, 'create'], array($attributes));
+    }
+
+    public function delete($id = null)
+    {
+        if (!empty($id)) { // 依据主键删除
+            if (is_array($id)) {
+                return call_user_func_array([$this->modelFullName, 'destroy'], [$id]);
+            }
+            return call_user_func_array([$this->modelFullName, 'destroy'], $id);
+        }
+
+        // 条件删除
+        $this->getQuery();
+        $ret = $this->query->delete();
+        $this->query = null;
+        return $ret;
+    }
+
+    public function update($id, $attributes)
+    {
+        $model = $this->findOrFail($id);
+        if (!$model) {
+            return false;
+        }
+
+        return $model->fill($attributes)->save();
+    }
+
+    public function __call($method, $parameters)
+    {
+        return call_user_func_array([$this->modelInstance, $method], $parameters);
+    }
+}
---
+++ src/BaseRepositoryContract.php
@@ -0,0 +1,263 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: szj
+ * Date: 16/8/21
+ * Time: 23:34
+ */
+namespace Zyts\Repositories;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+
+interface BaseRepositoryContract
+{
+    /**
+     * 添加一条错误消息
+     * @param string $errMsg
+     * @return mixed
+     */
+    public function addErrMsg($errMsg);
+
+    /**
+     * 获取最近一条错误消息
+     * @return mixed
+     */
+    public function getErrMsg();
+
+    public function getSqlDump();
+
+    /**
+     * 按主键id查询
+     * @param $id
+     * @param array $columns 查询列
+     * @return mixed
+     */
+    public function find($id, $columns = ['*']);
+
+    /**
+     * 按主键id查询，如果失败则返回false
+     * @param $id
+     * @param array $columns 查询列
+     * @return mixed
+     */
+    public function findOrFail($id, $columns = ['*']);
+
+    /**
+     * alias to simpleSelect
+     * @see simpleSelect
+     */
+    public function search($wheres, $columns = ['*'], $limits = 0, $orders = [], $groups = [], $havings = []);
+
+    /**
+     * 简单查询
+     *
+     * example:
+     *  // 基于字段多条件查询
+     *  repo->simpleSelect(['department' => 'test', 'age' => ['>', 30]]);
+     *
+     *  // 重复字段多条件查询
+     *  repo->simpleSelect([['age', '<', 50], ['age', '>', 30]]]);
+     *
+     *  // 仅查询个别字段
+     *  repo->simpleSelect(['department' => 'test'], ['age', 'name']);
+     *
+     *  // 限制条目数
+     *  repo->simpleSelect(['department' => 'test'], ['*'], 15); // 限制15条
+     *  repo->simpleSelect(['department' => 'test'], ['*'], [10, 15]); // 限制15条,从第11条开始
+     *
+     *  // 排序
+     *  repo->simpleSelect(['department' => 'test'], ['*'], 15, ['age' => 'desc']); // 按年龄正向排序
+     *
+     * @param array $wheres 条件
+     * @param array $columns 字段
+     * @param int|array $limits 限制
+     * @param array $orders 排序
+     * @param array $groups 分组
+     * @param array $havings 分组条件
+     * @return mixed
+     */
+    public function simpleSelect($wheres, $columns = ['*'], $limits = 0, $orders = [], $groups = [], $havings = []);
+
+    /**
+     * 列出所有
+     * @param array $columns 查询列
+     * @return mixed
+     */
+    public function all($columns = ['*']);
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function getQuery();
+
+    /**
+     * 添加 where and 条件
+     *
+     * example:
+     *
+     *  1) where department = 'test' and age > '30'
+     *  repo->wheres(['department' => 'test', 'age' => ['>', 30]]);
+     *
+     *  2) where age >= '18' and age < '30'
+     *  repo->wheres([
+     *      ['age', '>=', 18],
+     *      ['age', '<', 30]
+     *  ]);
+     *
+     * @param array $wheres 条件
+     * @return BaseRepositoryContract
+     */
+    public function wheres($wheres);
+
+    /**
+     * 添加 where or 条件
+     *
+     * example:
+     *
+     *  1) where department = 'test' or age > 30
+     *  repo->orWheres(['department' => 'test', 'age' => ['>', 30]]);
+     *  repo->orWheres([
+     *      ['department', '=', 'test'],
+     *      ['age', '>', 30]
+     *  ]);
+     *
+     *  2) where department in ('text', 'develop') or
+     *  repo->orWheres(['department' => ['in', ['test', 'develop']]]);
+     *  repo->orWheres([
+     *      ['department', 'in', ['test', 'develop']]
+     *  ]);
+     *
+     * @param array $wheres 条件
+     * @return BaseRepositoryContract
+     */
+    public function orWheres($wheres);
+
+    /**
+     * 添加查询字段
+     *
+     * example:
+     *
+     *  1) select columnA, columnB
+     *  repo->select(['columnA', 'columnB'])
+     *  repo->select('columnA, columnB')
+     *
+     *  2) select columnA as columnC, columnB as columnD
+     *  repo->select('columnA as columnC, columnB as columnD')
+     *
+     * @param array|numeric $columns 查询字段
+     * @return BaseRepositoryContract
+     */
+    public function select($columns);
+
+    /**
+     * 添加limit限制条件
+     *
+     * example:
+     *
+     *  1) limit 3 offset 1
+     *  repo->limits([1, 3])
+     *
+     *  2) limit 3
+     *  repo->limits(3)
+     *
+     * @param array|string $limits limit限制
+     * @return BaseRepositoryContract
+     */
+    public function limits($limits);
+
+    /**
+     * 添加排序条件
+     *
+     * example:
+     *
+     *  order by columnA asc, columnB desc
+     *  repo->orders(['columnA' => 'asc', 'columnB' => 'desc'])
+     *  repo->orders(['columnA asc', 'columnB desc'])
+     *
+     * @param array $orders 排序
+     * @return BaseRepositoryContract
+     */
+    public function orders($orders);
+
+    /**
+     * 添加分组条件
+     *
+     * example:
+     *
+     *  group by columnA asc, columnB desc
+     *  repo->orders(['columnA' => 'asc', 'columnB' => 'desc'])
+     *  repo->orders(['columnA asc', 'columnB desc'])
+     *
+     * @param array $groups 分组
+     * @return BaseRepositoryContract
+     */
+    public function groups($groups);
+
+    /**
+     * 添加分组过滤条件
+     *
+     * example:
+     *
+     *  having columnA > 3, columnB > 1
+     *  repo->havings([
+     *      ['columnA', '=', 3],
+     *      ['columnB', '=', 1]
+     *  ])
+     *
+     *  repo->havings(['columnA > 3', 'columnB > 1'])
+     * @param array $havings 分组过滤条件
+     * @return mixed
+     */
+    public function havings($havings);
+
+    /**
+     * 获取结果集合
+     *
+     * @return Collection
+     */
+    public function get();
+
+    /**
+     * 获取单个对象结果
+     *
+     * @return Model
+     */
+    public function first();
+
+    /**
+     * 获取查询结果个数
+     *
+     * @return integer
+     */
+    public function count();
+
+    /**
+     * 分页
+     *
+     * @param integer $numPerPage 每页多少
+     * @return mixed
+     */
+    public function paginate($numPerPage = 10);
+
+    /**
+     * 创建
+     * @param array $attributes 字段
+     * @return mixed
+     */
+    public function create($attributes);
+
+    /**
+     * 根据主键删除
+     * @param $id 主键
+     * @return mixed
+     */
+    public function delete($id = null);
+
+    /**
+     * 根据主键更新
+     * @param $id 主键
+     * @param array $attributes 字段
+     * @return mixed
+     */
+    public function update($id, $attributes);
+}
